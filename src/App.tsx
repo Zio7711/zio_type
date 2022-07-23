b3d91680c5c591f48b2c6c05921015b6368cfa22
Tan Ziao <zi.ao.tan@outlook.com>
2022-07-23 20:11:56 +0800

    create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+// import { useDispatch, useSelector } from "react-redux";
+// import { useEffect, useState } from "react";
+
+import Header from "components/Header";
+
+// import Test from "components/Test";
+// import Result from "components/Result";
+// import Footer from "components/Footer";
+// import { State } from "store/reducer";
+// import { setTimerId } from "store/actions";
+// import { recordTest } from "helpers/recordTest";
+// import "stylesheets/themes.scss";
+// import CommandPallet from "components/CommandPallet";
 
 function App() {
-  return <div>Hello World</div>;
+  return <Header />;
 }
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-// import todosReducer from '../features/todos/todosSlice'
-// import filtersReducer from '../features/filters/filtersSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './reducer';
 
 export const store = configureStore({
-  reducer: {
-    // todos: todosReducer,
-    // filters: filtersReducer
-  }
-})
+  reducer,
+});
